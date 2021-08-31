1ed68574b1de4e6773c94eb6f52025e858c88626
cynthia101488 <cynthia101488@gmail.com>
2021-08-31 11:46:30 +0800

    20210831 update

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const elemPage = document.querySelector('#Page');
 const elemLoad = document.querySelector('#Load');
-let data = '';
+let arr = [];
 let currentIndex = 0;
 
 setInit();
@@ -18,9 +18,9 @@ function getData() {
   const api = 'https://data.coa.gov.tw/Service/OpenData/ODwsv/ODwsvTravelFood.aspx'
   fetch(api)
   .then(res => res.json())
-  .then(originData => {
-    data = originData;
-    setPage(originData);
+  .then(data => {
+    dataSplit(data);
+    setPage(data);
     setTemplate(currentIndex);
     elemLoad.style = 'display:none';
   })
@@ -31,6 +31,12 @@ function getData() {
   })
 }
 
+function dataSplit(data) {
+  for (let i = 0, len = data.length; i < len; i += 10) {
+    arr.push(data.slice(i, i + 10));
+  }
+}
+
 function setPage(data) {
   let pageCode = '';
   let i = 0;
@@ -38,7 +44,7 @@ function setPage(data) {
     if (index % 10 === 0) {
       pageCode += `<button class="nav__page" type="button" data-id="${i}">${i + 1}</button>`;
       i += 1;
-  }
+    }
   });
   elemPage.innerHTML = pageCode;
   elemPage.children[currentIndex].classList.add('js-nav__page');
@@ -47,19 +53,18 @@ function setPage(data) {
 function setTemplate(startNum) {
   const elemTable = document.querySelector('#Table');
   let str = '';
-  let len = startNum + 10;
-  for (let i = startNum; i < len && data[i]; i++) {
+  for (let i = 0; i < arr[startNum].length; i++) {
     str += `<tr class="table__ls ${changeBgColor(i)}">
               <td class="table__item text-center text-gray">${i + 1}</td>
-              <td class="table__item">${data[i].City}</td>
+              <td class="table__item">${arr[startNum][i].City}</td>
               <td class="table__item">
                 <div class="img__inner">
-                  <img class="image" src="${data[i].PicURL}" alt="${data[i].Name}">
-                  <img class="image-hidden" src="${data[i].PicURL}" alt="${data[i].Name}">
+                  <img class="image" src="${arr[startNum][i].PicURL}" alt="${arr[startNum][i].Name}">
+                  <img class="image-hidden" src="${arr[startNum][i].PicURL}" alt="${arr[startNum][i].Name}">
                 </div>    
               </td>
-              <td class="table__item text-overflow">${data[i].Url ? `<a href="${data[i].Url}" target="_blank">${data[i].Name}</a>` : `${data[i].Name}`}</td>
-              <td class="table__item">${textLimit(data[i].HostWords)}</td>
+              <td class="table__item text-overflow">${arr[startNum][i].Url ? `<a href="${arr[startNum][i].Url}" target="_blank">${arr[startNum][i].Name}</a>` : `${arr[startNum][i].Name}`}</td>
+              <td class="table__item">${textLimit(arr[startNum][i].HostWords)}</td>
             </tr>`;
   }
   elemTable.innerHTML = str;
@@ -83,8 +88,7 @@ function doClick(e) {
   if (self.nodeName === 'BUTTON') {
     const prevIndex = currentIndex;
     currentIndex = parseInt(self.dataset.id);
-    const startIndex = currentIndex * 10;
-    setTemplate(startIndex);
+    setTemplate(currentIndex);
     setBtn(prevIndex)
   }
 }
