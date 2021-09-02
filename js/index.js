376da61871231229e100c7064866250c212e0003
cynthia101488 <76106817+cynthia101488@users.noreply.github.com>
2021-09-02 10:38:18 +0800

    20210902 update

--- js/index.js
+++ js/index.js
@@ -1,92 +1,115 @@
-const elemPage = document.querySelector('#Page');
-const elemLoad = document.querySelector('#Load');
-let arr = [];
-let currentIndex = 0;
-
-getData();
-setEvent();
-
-function getData() {
-  const api = 'https://data.coa.gov.tw/Service/OpenData/ODwsv/ODwsvTravelFood.aspx'
-  fetch(api)
-  .then(res => res.json())
-  .then(data => {
-    arr = dataSplit(data);
-    setPage(arr);
-    setTemplate(currentIndex);
-  })
-  .catch(err => {
-    if (err) {
-      alert('資料來源有誤!');
+class Food {
+  constructor() {
+    this.pageObject = {
+      arr: [],
+      currentIndex: 0,
+      prevIndex: 0,
+      rowSize: 10,
+      isLoad: true
     }
-  })
-  .finally(() => {
-    elemLoad.style = 'display:none';
-  });
-}
-
-function dataSplit(data) {
-  const rowSize = 10;
-  for (let i = 0, len = data.length; i < len; i += rowSize) {
-    arr.push(data.slice(i, i + rowSize));
+    
+    this.elemPage = document.querySelector('#Page');
+    this.elemTable = document.querySelector('#Table');
+    this.elemLoad = document.querySelector('#Load');
+    this.init();
   }
-  return arr;
-}
 
-function setPage(arr) {
-  let pageCode = '';
-  for (let i = 0, len = arr.length; i < len; i++) {
-    pageCode += `<button class="nav__page" type="button" data-id="${i}">${i + 1}</button>`;  
+  init() {
+    this.getData();
+    this.setEvent();
   }
-  elemPage.innerHTML = pageCode;
-  elemPage.children[currentIndex].classList.add('js-nav__page');
-}
 
-function setTemplate(index) {
-  const elemTable = document.querySelector('#Table');
-  let str = '';
-  const rowStartIndex = index * 10 + 1;
-  arr[index].forEach((item, i) => {
-    str += `<tr class="table__ls ${changeBgColor(i)}">
-              <td class="table__item text-center text-gray">${rowStartIndex + i}</td>
-              <td class="table__item">${item.City}</td>
-              <td class="table__item">
-                <div class="img__inner">
-                  <img class="image" src="${item.PicURL}" alt="${item.Name}">
-                  <img class="image-hidden" src="${item.PicURL}" alt="${item.Name}">
-                </div>    
-              </td>
-              <td class="table__item text-overflow">${item.Url ? `<a href="${item.Url}" target="_blank">${item.Name}</a>` : `${item.Name}`}</td>
-              <td class="table__item">${textLimit(item.HostWords)}</td>
-            </tr>`;
-  });
-  elemTable.innerHTML = str;
-}
+  setLoad() {
+    if (!this.pageObject.isLoad) {
+      this.elemLoad.style = 'display:none';
+    }
+  }
 
-function changeBgColor(index) {
-  return index % 2 === 0 ? '' : 'js-table__ls';
-}
+  getData() {
+    const api = 'https://data.coa.gov.tw/Service/OpenData/ODwsv/ODwsvTravelFood.aspx'
+    fetch(api)
+    .then(res => res.json())
+    .then(data => {
+      this.pageObject.arr = this.dataSplit(data);
+      this.setPage(this.pageObject.arr);
+      this.setTemplate(this.pageObject.currentIndex);
+    })
+    .catch(err => {
+      if (err) {
+        alert('資料來源有誤!');
+      }
+    })
+    .finally(() => {
+      this.pageObject.isLoad = false;
+      this.setLoad();
+    });
+  }
 
-function textLimit(text) {
-  let len = text.length;
-  return len > 50 ? text.substring(0,50) + '...' : text;
-}
+  dataSplit(data) {
+    for (let i = 0, len = data.length; i < len; i += this.pageObject.rowSize) {
+      this.pageObject.arr.push(data.slice(i, i + this.pageObject.rowSize));
+    }
+    return this.pageObject.arr;
+  }
 
-function setEvent() {
-  elemPage.addEventListener('click', doClick);
-}
+  setPage(arr) {
+    let pageCode = '';
+    for (let i = 0, len = arr.length; i < len; i++) {
+      pageCode += `<button class="nav__page" type="button" data-id="${i}">${i + 1}</button>`;  
+    }
+    this.elemPage.innerHTML = pageCode;
+    this.elemPage.children[this.pageObject.currentIndex].classList.add('js-nav__page');
+  }
 
-function doClick(e) {
-  const self = e.target;
-  if (self.nodeName === 'BUTTON') {
-    const prevIndex = currentIndex;
-    currentIndex = parseInt(self.dataset.id);
-    setTemplate(currentIndex);
-    setBtn(prevIndex)
+  setTemplate(index) {
+    const rowStartIndex = index * 10 + 1;
+    let str = '';
+    this.pageObject.arr[index].forEach((item, i) => {
+      str += `<tr class="table__ls ${this.changeBgColor(i)}">
+                <td class="table__item text-center text-gray">${rowStartIndex + i}</td>
+                <td class="table__item">${item.City}</td>
+                <td class="table__item">
+                  <div class="img__inner">
+                    <img class="image" src="${item.PicURL}" alt="${item.Name}">
+                    <img class="image-hidden" src="${item.PicURL}" alt="${item.Name}">
+                  </div>    
+                </td>
+                <td class="table__item text-overflow">${item.Url ? `<a href="${item.Url}" target="_blank">${item.Name}</a>` : `${item.Name}`}</td>
+                <td class="table__item">${this.textLimit(item.HostWords)}</td>
+              </tr>`;
+    });
+    this.elemTable.innerHTML = str;
+  }
+
+  changeBgColor(index) {
+    return index % 2 === 0 ? '' : 'js-table__ls';
+  }
+  
+  textLimit(text) {
+    let len = text.length;
+    return len > 50 ? text.substring(0,50) + '...' : text;
+  }
+
+  setEvent() {
+    this.elemPage.addEventListener('click', (e) => {
+      this.doClick(e)
+    });
+  }
+
+  doClick(e) {
+    const self = e.target;
+    if (self.nodeName === 'BUTTON') {
+      this.pageObject.prevIndex = this.pageObject.currentIndex;
+      this.pageObject.currentIndex = parseInt(self.dataset.id);
+      this.setTemplate(this.pageObject.currentIndex);
+      this.setBtn(this.pageObject.prevIndex);
+    }
+  }
+  
+  setBtn(index) {
+    this.elemPage.children[this.pageObject.currentIndex].classList.add('js-nav__page');
+    this.elemPage.children[index].classList.remove('js-nav__page');
   }
 }
 
-function setBtn(index) {
-  elemPage.children[currentIndex].classList.add('js-nav__page');
-  elemPage.children[index].classList.remove('js-nav__page');
-}
+const food = new Food();
